c660c5ffd8323aac97795ce3e4715efc728b4b84
Johnny-sel <68633595+Johnny-sel@users.noreply.github.com>
2023-01-25 11:04:28 +0300

    Delete Types.ts

--- src/app/pages/EditPasswordPage/Types.ts
+++
@@ -1,9 +0,0 @@
-import {UpdatePasswordRequest} from '@api/types';
-
-export type EditPasswordState = {
-  load: boolean;
-  error?: string;
-  data: UpdatePasswordRequest;
-  target?: HTMLButtonElement;
-  event?: InputEvent;
-};
