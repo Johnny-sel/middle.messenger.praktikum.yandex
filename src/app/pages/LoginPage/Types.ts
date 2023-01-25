bd916e62876d37172216a73cce424e4f3019fdb7
Johnny-sel <68633595+Johnny-sel@users.noreply.github.com>
2023-01-25 11:02:59 +0300

    Delete Types.ts

--- src/app/pages/LoginPage/Types.ts
+++
@@ -1,9 +0,0 @@
-import {SigninRequest} from '@api/types';
-
-export type LoginState = {
-  load: boolean;
-  error?: string;
-  data: SigninRequest;
-  target?: HTMLButtonElement;
-  event?: InputEvent;
-};
